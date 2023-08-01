4c480fe36ed9c58893b9252660e4265d4426bbe5
DR-Rajasekhar <rajasekhar.gurugubelli@devrabbit.com>
2023-08-01 10:16:39 +0530

    modified app.py

--- python-flask/app.py
+++ python-flask/app.py
@@ -1,7 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route('/')
 def hello_geek():
    return '<h1>Hello from Flask & Docker</h1>'
+@app.route('/ms')
+@app.route('/ps')
+@app.route('/mps')
+def mega():
+   return '<h1>Welcome to MegaFamily!......</h1>'
+@app.route('/ss')
+def super():
+   return '<h1>Welcome to SuperstarFamily</h1>'
+
+
 if __name__ == "__main__":
    app.run(debug=True)
